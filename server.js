096ae211b2891e7500d2ba37905dd964d293f6f1
Dinesh <dinesh@botamation.in>
2025-04-19 21:23:01 +0530

    Add gRPC server implementation and update start script

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
 
-const PROTO_PATH = path.join(__dirname, '../protos/notification.proto');
+const PROTO_PATH = path.join(__dirname, './protos/notification.proto');
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const notificationProto = grpc.loadPackageDefinition(packageDefinition);
